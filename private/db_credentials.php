5a3e9f06d2b2e0b4eb356ab237c93d090562fd56
Md Anwar Jamal <cse.mdanwarjamal@gmail.com>
2020-12-03 10:10:15 +0530

    Create and add DB Credentials

---
+++ private/db_credentials.php
@@ -0,0 +1,7 @@
+<?php
+  //Keep db credentials in separate file
+  define('DB_SERVER', 'localhost');
+  define('DB_USER', 'webuser');
+  define('DB_PASS', 'secretpassword');
+  define('DB_NAME', 'chain_gang');
+?>
